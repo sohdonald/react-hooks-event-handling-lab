f53903cb3aa379da7d64d0dd11a41f61e37db34b
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-08-22 11:24:51 -0400

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
+import React from "react";
 // Code Keypad Component Here
+//let password = "trabPuKcip";
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password"
+            onChange={() => {
+                console.log('Entering password...')
+            }}>
+
+            </input>
+        </div>
     )
 }
 
